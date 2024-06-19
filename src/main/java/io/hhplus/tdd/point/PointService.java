005baa1e28c8dd136fa504782234a634d3040f1a
JiheeOh <jenny2596@naver.com>
2024-06-19 23:46:42 +0900

    :feature : 포인트 사용 기능 추가
     - 포인트를 사용하는 기능 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -14,7 +14,7 @@ public class PointService {
     public UserPoint getPoint(long id) {
         UserPoint result = userPointRepository.selectById(id);
         if (result.updateMillis() == 0) {
-            throw new UserNotFoundException();
+            throw new NoUserException();
         }
         return result;
     }
@@ -23,9 +23,24 @@ public class PointService {
         UserPoint result = userPointRepository.selectById(id);
         // 기존 사용자임
         if (result.updateMillis() != 0) {
-            amount = result.point()+ amount;
+            amount = result.point() + amount;
         }
 
         return userPointRepository.insertOrUpdate(id, amount);
+    }
+
+    public UserPoint usePoint(long id, long amount) {
+        UserPoint existPoint = userPointRepository.selectById(id);
+
+        if (existPoint.updateMillis() == 0) {
+            throw new NoUserException();
+        } else if (existPoint.point() - amount < 0) {
+            throw new NoPointException();
+        }
+
+        UserPoint result = new UserPoint(id, existPoint.point() - amount, System.currentTimeMillis());
+        userPointRepository.insertOrUpdate(result.id(), result.point());
+
+        return result;
     }
 }
