b3c1821dbf7608f4547a73155823fb3c903057fc
JiheeOh <jenny2596@naver.com>
2024-06-19 22:15:45 +0900

    : feature : 포인트 조회 및 적립 기능 추가
    - Controller, Service 기능 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,19 +1,31 @@
 package io.hhplus.tdd.point;
 
-import io.hhplus.tdd.database.UserPointTable;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class PointService {
 
-    private final UserPointTable userPointTable;
+    private final UserPointRepository userPointRepository;
 
-    public PointService(UserPointTable userPointTable){
-        this.userPointTable = userPointTable;
+    public PointService(UserPointRepository userPointRepository) {
+        this.userPointRepository = userPointRepository;
     }
+
     public UserPoint getPoint(long id) {
-        return userPointTable.selectById(id);
+        UserPoint result = userPointRepository.selectById(id);
+        if (result.updateMillis() == 0) {
+            throw new UserNotFoundException();
+        }
+        return result;
+    }
+
+    public UserPoint charge(long id, long amount) {
+        UserPoint result = userPointRepository.selectById(id);
+        // 기존 사용자임
+        if (result.updateMillis() != 0) {
+            amount = result.point()+ amount;
+        }
+
+        return userPointRepository.insertOrUpdate(id, amount);
     }
 }
