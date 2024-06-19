33cda6f23aaadc2a0f7aeedf3f2d183d9763e955
JiheeOh <jenny2596@naver.com>
2024-06-19 22:18:13 +0900

    :refactor : 등록되지 않은 사용자 처리 코드 추가
    - 등록되지 않은 사용자의 포인트를 조회 시
    UserNotFoundException으로 처리하도록 변경
    
    - 등록되지 않은 사용자와 포인트가 0인 기존 사용자를 구분하기 위해 updateMillis를 이용함
    UpdateMillis == 0 -> 등록되지 않은 사용자
    UpdateMilis == System.currentMillis() -> 등록된 사용자

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -6,7 +6,8 @@ public record UserPoint(
         long updateMillis
 ) {
 
+    // 등록이 안되어있던 사용자를 구분하기 위해 updateMillis를 0으로 설정
     public static UserPoint empty(long id) {
-        return new UserPoint(id, 0, System.currentTimeMillis());
+        return new UserPoint(id,0,0);
     }
 }
