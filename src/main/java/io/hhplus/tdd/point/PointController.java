4c89d0245512e9fb92398038c552eb6ef4aa7530
JiheeOh <jenny2596@naver.com>
2024-06-20 01:32:58 +0900

    :style : TODO 주석 삭제

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -17,9 +17,7 @@ public class PointController {
         this.pointService = pointService;
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
+
     @GetMapping("{id}")
     public UserPoint point(
             @PathVariable long id
@@ -27,9 +25,7 @@ public class PointController {
         return pointService.getPoint(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
+
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
             @PathVariable long id
@@ -37,9 +33,7 @@ public class PointController {
         return pointService.history(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
+
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
@@ -48,9 +42,7 @@ public class PointController {
         return pointService.charge(id,amount);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
+
     @PatchMapping("{id}/use")
     public UserPoint use(
             @PathVariable long id,
